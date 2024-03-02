00e824eb74fe676cae686f916c80ef66406d5793
Marcos Euzébio <markeuzebio@gmail.com>
2024-03-01 21:57:48 -0300

    Add UI, turning the game playable via buttons
    User chooses their option via prompt no more.
    Now, buttons are utilized to choose rock, paper
    or scissors just by clicking on the one desired.
    
    The logic that checks who won the game changed.
    Now, the first entity to reach 5 points wins.
    This eliminates the possibility of a tie.

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+let player_count;
+let computer_count;
+
+player_count = 0;
+computer_count = 0;
+
+const body = document.querySelector('body');
+const div_result = document.createElement('div');
+const btn_rock = document.createElement('button');
+const btn_paper = document.createElement('button');
+const btn_scissors = document.createElement('button');
+
+btn_rock.textContent = 'Rock';
+btn_paper.textContent = 'Paper';
+btn_scissors.textContent = 'Scissors';
+
+btn_rock.addEventListener('click', () => playSingleRound('rock', getComputerChoice()));
+btn_paper.addEventListener('click', () => playSingleRound('paper', getComputerChoice()));
+btn_scissors.addEventListener('click', () => playSingleRound('scissors', getComputerChoice()));
+
+body.appendChild(btn_rock);
+body.appendChild(btn_paper);
+body.appendChild(btn_scissors);
+body.appendChild(div_result);
+
 /*
     Returns either rock, paper or scissors.
 */
@@ -20,74 +45,77 @@ function getComputerChoice() {
     Returns 2 if there is a tie.
 */
 function playSingleRound(player_selection, computer_selection) {
+    if(player_count == 5 || computer_count == 5)
+        return;
+
+    let return_value;
+
     player_selection = player_selection.toLowerCase();
 
     if(player_selection == "rock") {
-        if(computer_selection == "scissors")
-            return 0;
+        if(computer_selection == "scissors") {
+            document.querySelector('div').innerHTML += "<p>You Win! " + player_selection + " beats " + computer_selection + "</p>";
+            player_count++;
+            return_value = 0;
+        }
 
-        else if(computer_selection == "paper")
-            return 1;
+        else if(computer_selection == "paper") {
+            document.querySelector('div').innerHTML += "<p>You Lose! " + computer_selection + " beats " + player_selection + "</p>";
+            computer_count++;
+            return_value = 1;
+        }
 
-        else if(computer_selection == "rock")
-            return 2;
+        else {
+            document.querySelector('div').innerHTML += "<p>A Tie happened!</p>";
+            return_value = 2;
+        }
 
     }
 
     else if(player_selection == "paper") {
-        if(computer_selection == "rock")
-            return 0;
-
-        else if(computer_selection == "scissors")
-            return 1;
-
-        else if(computer_selection == "paper")
-            return 2;
-    }
-
-    else if(player_selection == "scissors") {
-        if(computer_selection == "paper")
-            return 0;
-
-        else if(computer_selection == "rock")
-            return 1;
-
-        else if(computer_selection == "scissors")
-            return 2;
-    }
-}
-
-function playGame() {
-    let player_selection;
-    let computer_selection;
-    let player_count;
-    let computer_count;
-    let result;
-    
-    player_count = 0;
-    computer_count = 0;
-
-    for(let i = 1 ; i <= 5 ; i++) {
-        player_selection = prompt("Rock, Paper or Scissors?");
-        computer_selection = getComputerChoice();
-        result = playSingleRound(player_selection, computer_selection);
-        if(result == 0) {
-            console.log("You Win! " + player_selection + " beats " + computer_selection);
+        if(computer_selection == "rock") {
+            document.querySelector('div').innerHTML += "<p>You Win! " + player_selection + " beats " + computer_selection + "</p>";
             player_count++;
+            return_value = 0;
         }
-        else if(result == 1) {
-            console.log("You Lose! " + computer_selection + " beats " + player_selection);
+
+        else if(computer_selection == "scissors") {
+            document.querySelector('div').innerHTML += "<p>You Lose! " + computer_selection + " beats " + player_selection + "</p>";
             computer_count++;
+            return_value = 1
         }
-        else if(result == 2) {
-            console.log("A Tie happened!");
+
+        else {
+            document.querySelector('div').innerHTML += "<p>A Tie happened!</p>";
+            return_value = 2;
         }
     }
-        
-    if(player_count > computer_count)
-        console.log("You won the game!");
-    else if(player_count < computer_count)
-        console.log("You lost the game!");
-    else if(player_count == computer_count)
-        console.log("It was a tie!");
+
+    else {
+        if(computer_selection == "paper") {
+            document.querySelector('div').innerHTML += "<p>You Win! " + player_selection + " beats " + computer_selection + "</p>";
+            player_count++;
+            return_value = 0;
+        }
+
+        else if(computer_selection == "rock") {
+            document.querySelector('div').innerHTML += "<p>You Lose! " + computer_selection + " beats " + player_selection + "</p>";
+            computer_count++;
+            return_value = 1;
+        }
+
+        else {
+            document.querySelector('div').innerHTML += "<p>A Tie happened!</p>";
+            return_value = 2;
+        }
+    }
+
+    if(player_count == 5 || computer_count == 5) {
+        if(player_count > computer_count)
+            document.querySelector('div').innerHTML += "<p>You Won!</p>";
+        else if(computer_count > player_count)
+            document.querySelector('div').innerHTML += "<p>You Lose!</p>";
+    }
+
+    return return_value;
 }
